5cbb2a1b385946b5c23d7f7bb3880f3cb2540eb9
NJLin <765980891@qq.com>
2018-04-12 15:15:08 +0800

    merge

--- functionBar/FunctionAccountWidget.h
+++ functionBar/FunctionAccountWidget.h
@@ -15,16 +15,13 @@ public:
     explicit FunctionAccountWidget(QWidget *parent = 0);
     ~FunctionAccountWidget();
 private slots:
-<<<<<<< HEAD
     void on_minerInfoBtn_clicked();
+    void AssetShowSlots();
+    void MinerShowSlots();
 
 signals:
     void showMinerSignal();
 
-=======
-    void AssetShowSlots();
-    void MinerShowSlots();
->>>>>>> bb3273ae82a92d28cc7d77c7cfdcc1f04d525643
 private:
     void InitWidget();
     void InitStyle();
