f5ed420b20406c4bd9fec685571ed16553156b78
朱正天 <47029316@qq.comm>
2018-04-26 17:17:25 +0800

    修改多签地址获取

--- capitalTransferPage/CapitalTransferDataUtil.cpp
+++ capitalTransferPage/CapitalTransferDataUtil.cpp
@@ -33,12 +33,12 @@ QString CapitalTransferDataUtil::parseMutiAddress(const QString &jsonString)
     if(json_error.error != QJsonParseError::NoError || !parse_doucment.isObject()) return "";
     QJsonObject jsonObject = parse_doucment.object();
     qDebug()<<jsonString;
-    if(!jsonObject.value("result").isArray())
+    if(!jsonObject.value("result").isObject())
     {
         return "";
     }
 
-    return jsonObject.value("result").toArray().last().toObject().value("bind_account_hot").toString();
+    return jsonObject.value("result").toObject().value("bind_account_hot").toString();
 }
 
 QJsonObject CapitalTransferDataUtil::parseTradeDetail(const QString &jsonString)
