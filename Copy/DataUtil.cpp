7563ad4ab1dc0c4c3aaa853665bbc896ed3efdce
NJLin <765980891@qq.com>
2018-11-20 11:33:10 +0800

    copy logfile name

--- Copy/DataUtil.cpp
+++ Copy/DataUtil.cpp
@@ -173,9 +173,9 @@ void DataUtil::myMessageOutput(QtMsgType type, const QMessageLogContext &context
     QString strMessage = QString("DateTime:%1 Message:%2").arg(strDateTime).arg(localMsg.constData());
 
     // 输出信息至文件中（读写、追加形式），超过50M删除日志
-    QFileInfo info("log.txt");
-    if(info.size() > 1024*1024*50) QFile::remove("log.txt");
-    QFile file("log.txt");
+    QFileInfo info("update_log.txt");
+    if(info.size() > 1024*1024*50) QFile::remove("update_log.txt");
+    QFile file("update_log.txt");
     file.open(QIODevice::ReadWrite | QIODevice::Append);
     QTextStream stream(&file);
     stream << strMessage << "\r\n";
--- LNKWallet/wallet.cpp
+++ LNKWallet/wallet.cpp
@@ -167,7 +167,8 @@ void HXChain:: startExe()
 
     QStringList strList;
     strList << QString("--data-dir=\"%1\"").arg(HXChain::getInstance()->configFile->value("/settings/chainPath").toString().replace("\\","/"))
-            << QString("--rpc-endpoint=127.0.0.1:%1").arg(NODE_RPC_PORT);
+            << QString("--rpc-endpoint=127.0.0.1:%1").arg(NODE_RPC_PORT)
+            << "--rewind-on-close";
 
     if( HXChain::getInstance()->configFile->value("/settings/resyncNextTime",false).toBool())
     {
