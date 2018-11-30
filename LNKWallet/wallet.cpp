1e41a8d3d2440446881acb3a6143e1fb0e5686e0
thomas <47029316@qq.com>
2018-11-30 17:33:23 +0800

    test

--- LNKWallet/wallet.cpp
+++ LNKWallet/wallet.cpp
@@ -419,7 +419,7 @@ void HXChain::getSystemEnvironmentPath()
         if (str.startsWith("APPDATA="))
         {
 
-            walletConfigPath = str.mid(8) + "\\HXIndicator" WALLET_EXE_SUFFIX "_" "1.1.0_tt";
+            walletConfigPath = str.mid(8) + "\\HXIndicator" WALLET_EXE_SUFFIX "_" "1.1.0";
             appDataPath = walletConfigPath + "\\chaindata";
             qDebug() << "appDataPath:" << appDataPath;
             break;
