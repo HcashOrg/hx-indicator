409a780ddc4cf9931269518bc53ac208a3e1a358
NJLin <765980891@qq.com>
2019-03-05 16:49:00 +0800

    dbreplay

--- LNKWallet/wallet.cpp
+++ LNKWallet/wallet.cpp
@@ -205,12 +205,12 @@ void HXChain:: startExe()
             ;
 
     if( HXChain::getInstance()->configFile->value("/settings/resyncNextTime",false).toBool()
-            ||  HXChain::getInstance()->configFile->value("/settings/dbReplay7",true).toBool())
+            ||  HXChain::getInstance()->configFile->value("/settings/dbReplay8",true).toBool())
     {
         strList << "--replay";
     }
     HXChain::getInstance()->configFile->setValue("/settings/resyncNextTime",false);
-    HXChain::getInstance()->configFile->setValue("/settings/dbReplay7",false);
+    HXChain::getInstance()->configFile->setValue("/settings/dbReplay8",false);
 
     nodeProc->start(NODE_PROC_NAME,strList);
     qDebug() << "start" << NODE_PROC_NAME << strList;
